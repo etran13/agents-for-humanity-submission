4f216cc36f3b21bbf37d71e906108bfd854088b1
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-08-31 23:04:59 -0500

    Fix test file

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -1,17 +1,39 @@
-#Imports
+# Run this file to verify that the Strands Agent SDK is working.
+
+# Imports
+from dotenv import load_dotenv
+import os
+
 from strands import Agent, tool
+from strands.models.openai import OpenAIModel
+from strands_tools import calculator
 
-# Run this to verify that the Strands Agent SDK is working.
+#Load the .env file with the API key.
+load_dotenv()
+api_key = os.getenv("API_KEY")
 
+#Define the model (needed to connect to OpenAI API)
+model = OpenAIModel(
+    client_args={
+        "api_key": api_key,
+    },
+    # **model_config
+    model_id="gpt-4o",
+    params={
+        "max_tokens": 100,
+        "temperature": 0.7,
+    }
+)
 
 #Define tools that the agent can use.
+#The docstring lets the agent know what the tool does. 
 @tool
 def word_count(text: str) -> int:
-    """Count words in text.
+    """Given word, return letter count
 
-    This docstring is used by the LLM to understand the tool's purpose.
     """
     return len(text.split())
 
-agent = Agent(tools=[word_count])
-response = agent("How many words are in this sentence?")
+print("Agent starting...")
+agent = Agent(model=model, tools=[word_count])
+response = agent("How many letters in 'strawberry'?")
