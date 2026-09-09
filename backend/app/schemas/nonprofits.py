95683ac6827b054cde75cdd9cf794536a873b137
Ngoc <huynhnhan254@gmail.com>
2026-09-08 22:44:28 -0500

    update nonprofit schema

--- backend/app/schemas/nonprofits.py
+++ backend/app/schemas/nonprofits.py
@@ -1,11 +1,25 @@
-from pydantic import BaseModel, HttpUrl, EmailStr
+from datetime import date
+from pydantic import BaseModel, EmailStr, HttpUrl
+
 
 class NonprofitCreate(BaseModel):
     organization_name: str
+    organization_type: str
+
     website: HttpUrl
     email: EmailStr
+
     description: str
-    location: str
+    city: str
+    state: str
+
     focus_area: list[str]
     population_served: str
     funding_need: str
+
+    organization_size: str
+    geographic_service_area: str
+
+    project_budget: float
+    project_start_date: date
+    project_end_date: date
